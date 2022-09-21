5f0b1c0ac4e03d5fbec6e89a71db1e266f89cd9d
Deepak <deepakgunpal0@gmail.com>
2022-09-21 15:55:57 +0530

    catch response added in task 3

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -77,7 +77,7 @@ const ethereumPriceAndBalance = async (req, res) => {
         const currBalAndPrice = await balAndPriceModel.create(data);
         res.status(201).send({ status: true, data: currBalAndPrice });
     } catch (error) {
-
+        res.status(500).send({ status: false, message: error.message });
     }
 }
 
